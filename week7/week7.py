395f376de76481e69cb1609a5b027d419875261c
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-03-28 20:51:19 -0400

    fixed it

--- week7/week7.py
+++ week7/week7.py
@@ -1,9 +1,11 @@
+import string
+from unicodedata import normalize
 total = 0
 dim = list()
-with open("devClub/week7/phrases.txt", "r") as phrases:
+with open("week7/phrases.txt", "r") as phrases:
     dim = phrases.read().splitlines()
 for phrase in dim:
-    if(len(list(dict.fromkeys(list(phrase.replace(" ",""))))) == 26):
+    if(len(list(dict.fromkeys(list(normalize("NFD",phrase.translate(str.maketrans('', '', string.punctuation)).replace(" ",'')).lower())))) == 26):
         total+=1
 
 print(total)
