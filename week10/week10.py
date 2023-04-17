9e547e5da72e1614b5945ebd2b7f5d8e6342a1a3
Pierre-Luc Robitaille <plrobitaille@classeaffaires.com>
2023-04-17 15:21:55 -0400

    mise a jour et cleanup

--- week10/week10.py
+++ week10/week10.py
@@ -2,6 +2,8 @@ def XOR(chunk0, chunk1):
     return bytes(a ^ b for a, b in zip(chunk0, chunk1))
 
 
+open('week10/disk1new.bin', 'wb').close()
+open('week10/newImage.png', 'wb').close()
 with open("week10/disk0.bin", mode="rb") as disk0, open("week10/disk2.bin", mode="rb") as disk2:
     chunk0 = disk0.read(256)
     chunk2 = disk2.read(256)
@@ -11,29 +13,24 @@ with open("week10/disk0.bin", mode="rb") as disk0, open("week10/disk2.bin", mode
         chunk0 = disk0.read(256)
         chunk2 = disk2.read(256)
 
-layers = 0
+
 with open("week10/disk0.bin", mode="rb") as disk0, open("week10/disk1new.bin", mode="rb") as disk1, open("week10/disk2.bin", mode="rb") as disk2:
+    layers = 0
     chunk0 = disk0.read(256)
     chunk1 = disk1.read(256)
     chunk2 = disk2.read(256)
     while chunk0 and chunk1 and chunk2:
         if (layers % 3 == 0):
             with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk0)
-            with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk1)
+                newImage.write(chunk0+chunk1)
 
         if (layers % 3 == 1):
             with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk0)
-            with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk2)
+                newImage.write(chunk0+chunk2)
 
         if (layers % 3 == 2):
             with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk1)
-            with open("week10/newImage.png", "ab") as newImage:
-                newImage.write(chunk2)
+                newImage.write(chunk1+chunk2)
 
         chunk0 = disk0.read(256)
         chunk1 = disk1.read(256)
